d40a39d7c673be3d919cc513364db3bd16a735a9
Ken Gorab <ken.gorab@gmail.com>
2021-01-03 17:49:54 -0500

    Initial commit, w/ working grammar
    - Working grammar!

---
+++ examples/sample.ts
@@ -0,0 +1,22 @@
+interface Abc {
+
+}
+
+const def = 123;
+var ghi = 123;
+console.log(def);
+
+function abc(a: Abc[]): Abc[] {
+  const def = 123;
+  console.log(def);
+  return null
+}
+
+let x = []
+let a = abc(x).length
+
+let Class = 123
+
+function xyz<T>(a: T) {
+ // asdf
+}
