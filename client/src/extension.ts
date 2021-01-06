bf8319f8d3546d80803959239bec8e6c78601de0
Ken Gorab <ken.gorab@gmail.com>
2021-01-05 19:14:54 -0500

    Real LSP implementation
    - Rather than use the node implementation (which used a
    temporarily-built nodejs wasm build of @kengorab/abra_wam), this
    actually uses the `abra-lsp` binary from the abra project. It's written
    in rust, and integrates directly with the abra compiler, so it feels a
    lot more native and natural to use.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,26 +1,46 @@
 import * as path from 'path';
-import { ExtensionContext } from 'vscode';
-import { LanguageClient, TransportKind } from 'vscode-languageclient';
+import { ExtensionContext, workspace } from 'vscode';
+import { LanguageClient } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	const serverOptions = { module: serverModule, transport: TransportKind.ipc };
+  workspace.onDidChangeConfiguration(() => {
+    console.log('config changed!');
+    
+    client?.stop();
+    client = init(context);
+    client.start();
+  });
+
+  client = init(context);
+	client.start();
+}
+
+export function deactivate() {
+  if (!client) return;
+	return client.stop();
+}
+
+function init(context: ExtensionContext) {
+  const command = getLspCommand(context);
+  const serverOptions = { command };
 	const clientOptions = { documentSelector: [{ scheme: 'file', language: 'abra' }] };
 
-	client = new LanguageClient(
+	return new LanguageClient(
 		'abra-language-server',
 		'Abra Language Server',
 		serverOptions,
 		clientOptions
 	);
-	client.start();
 }
 
-export function deactivate() {
-  if (!client) return
-	return client.stop()
+function getLspCommand(context: ExtensionContext): string {
+  const config = workspace.getConfiguration('abra');
+
+  if (config.has('lspPath') && config.get('lspPath')) {
+    return config.get('lspPath');
+  } else {
+    return context.asAbsolutePath(path.join('bin', 'abra-lsp'));
+  }
 }
