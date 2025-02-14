0799f59613b74f6df1b34a1104a1f41c38302cae
Ken Gorab <ken.gorab@gmail.com>
2025-02-13 20:47:33 -0500

    Prepare for selfhosted LSP
    This adds some wiring for the selfhosted language service, as well as
    some new (but still incomplete!) rules for the language syntax.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,15 +1,16 @@
 import * as path from 'path';
 import { ExtensionContext, workspace } from 'vscode';
-import { LanguageClient } from 'vscode-languageclient';
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-  workspace.onDidChangeConfiguration(() => {
-    client?.stop();
-    client = init(context);
-    client.start();
-  });
+  // TODO: uncomment when LSP supports `shutdown` message
+  // workspace.onDidChangeConfiguration(() => {
+  //   client?.stop();
+  //   client = init(context);
+  //   client.start();
+  // });
 
   client = init(context);
 	client.start();
@@ -22,23 +23,31 @@ export function deactivate() {
 
 function init(context: ExtensionContext) {
   const command = getLspCommand(context);
-  const serverOptions = { command };
-	const clientOptions = { documentSelector: [{ scheme: 'file', language: 'abra' }] };
 
 	return new LanguageClient(
 		'abra-language-server',
 		'Abra Language Server',
-		serverOptions,
-		clientOptions
+		{
+      command,
+      transport: TransportKind.stdio,
+    },
+		{
+      documentSelector: [
+        { scheme: 'file', language: 'abra' }
+      ]
+    }
 	);
 }
 
 function getLspCommand(context: ExtensionContext): string {
-  const config = workspace.getConfiguration('abra');
+  // const config = workspace.getConfiguration('abra');
 
-  if (config.has('lspPath') && config.get('lspPath')) {
-    return config.get('lspPath');
-  } else {
-    return context.asAbsolutePath(path.join('bin', 'abra-lsp'));
-  }
+  // if (config.has('lspPath') && config.get('lspPath')) {
+  //   return config.get('lspPath');
+  // } else {
+  //   return context.asAbsolutePath(path.join('bin', 'abra-lsp'));
+  // }
+
+  // TODO: don't hardcode
+  return '/Users/kengorab/Desktop/abra-lang/projects/lsp/._abra/abra-lsp'
 }
